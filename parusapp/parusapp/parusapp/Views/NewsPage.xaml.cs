a672d3eda06c1825815ed116d926230c8c111eff
slepovep <slepov.ep@gmail.com>
2020-09-20 13:55:56 +0300

    Добавлена страница новости с отображением веб страницы сайта

---
+++ parusapp/parusapp/parusapp/Views/NewsPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Xamarin.Forms;
+
+namespace parusapp.Views
+{
+	public partial class NewsPage : ContentPage
+	{
+		public NewsPage()
+		{
+			InitializeComponent ();
+        }
+        void webviewNavigating(object sender, WebNavigatingEventArgs e)
+        {
+            labelLoading.IsVisible = true;
+        }
+
+        void webviewNavigated(object sender, WebNavigatedEventArgs e)
+        {
+            labelLoading.IsVisible = false;
+        }
+    }
+}
+
