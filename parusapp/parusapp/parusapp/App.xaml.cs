4a32dcc5307ed60f2f82c34f5d574120c41a1416
slepovep <slepov.ep@gmail.com>
2020-09-29 00:45:51 +0300

    изменился какой-то исполняемый код

--- parusapp/parusapp/parusapp/App.xaml.cs
+++ parusapp/parusapp/parusapp/App.xaml.cs
@@ -46,6 +46,28 @@ namespace parusapp
                 return database;
             }
         }
+        /*      public static EventRepository Database {
+                  get {
+                      if (database == null) {
+                          // путь, по которому будет находиться база данных
+                          string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), DATABASE_NAME);
+                          // если база данных не существует (еще не скопирована)
+                          if (!File.Exists(dbPath)) {
+                              // получаем текущую сборку
+                              var assembly = IntrospectionExtensions.GetTypeInfo(typeof(App)).Assembly;
+                              // берем из нее ресурс базы данных и создаем из него поток
+                              using (Stream stream = assembly.GetManifestResourceStream($"parusapp.{DATABASE_NAME}")) {
+                                  using (FileStream fs = new FileStream(dbPath, FileMode.OpenOrCreate)) {
+                                      stream.CopyTo(fs);  // копируем файл базы данных в нужное нам место
+                                      fs.Flush();
+                                  }
+                              }
+                          }
+                          database = new EventRepository(dbPath);
+                      }
+                      return database;
+                  }
+              }*/
         public App()
 		{
 			Xamarin.Forms.DataGrid.DataGridComponent.Init();
